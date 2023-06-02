e931e00d2eb284764d4069c86a32b7a427b0de70
chrisi5700 <chrisi57001@gmail.com>
2023-06-02 17:43:53 +0200

    finally working workspace

--- relational_algebra.cpp
+++ relational_algebra.cpp
@@ -1,4 +1,11 @@
 #include <iostream>
+#include <memory>
+
+std::unique_ptr<int[]> func()
+{
+    std::unique_ptr<int[]> arr;
+    return arr;
+}
 
 void say_hello(){
     std::cout << "Hello, from relational_algebra!\n";
